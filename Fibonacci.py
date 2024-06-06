14133a7013fdd9fd6c011f499f6ed4f97ec874d1
Jeffreyarul <jeffreyarul@karunya.edu.in>
2024-06-06 22:58:00 +0530

    chang succesful

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,3 +1,8 @@
-def main():
-    print("def")
-    return
+def fibonacciSeries(i):
+	if i<=1:
+		    return i 
+	else :
+		    return (fibonacciSeries(i - 1) + fibonacciSeries(i - 2))
+num=10
+for i in range(num):
+	            print(fibonacciSeries(i), end=" ")    
